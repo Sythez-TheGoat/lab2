b3eb5c90f5f16093b6e9155c05fab16d78beb12b
kaungkaung <kaunghtoo764@gmail.com>
2026-07-19 14:24:08 +0700

    finished 8.7

---
+++ src/components/EventMeta.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-meta">
+    <span>{{ event.category }}</span>
+    <span>{{ event.organizer }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-meta {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  font-size: 14px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
+import EventMeta from '@/components/EventMeta.vue'
 import type { Event } from '@/types'
 import { ref } from 'vue'
 
@@ -41,7 +42,18 @@ const events = ref<Event[]>([
 </script>
 
 <template>
-  <div class="home">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  <div class="events">
+    <div v-for="event in events" :key="event.id" class="event-row">
+      <EventCard :event="event" />
+      <EventMeta :event="event" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
