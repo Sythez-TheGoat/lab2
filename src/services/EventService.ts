b73300cfa4c7efbe6577d37e51ec7e35768dedd9
kaungkaung <kaunghtoo764@gmail.com>
2026-09-07 16:31:50 +0700

    chainging to localhost 8080

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Sythez-TheGoat/lab2',
+  baseURL: 'http://localhost:8080',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
