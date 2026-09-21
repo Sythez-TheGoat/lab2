c0d57ea9db2420c7fe304908de7f44580b86ecb2
kaungkaung <kaunghtoo764@gmail.com>
2026-09-21 11:13:36 +0700

    Finished step 5 lab9

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const modelValue = defineModel()
+interface BaseInputProps {
+  label: string
+}
+const props = withDefaults(defineProps<BaseInputProps>(), {
+  label: ''
+})
+</script>
+<template>
+  <label v-if="props.label" class="block text-gray-500 font-bold">
+    {{ props.label }}
+  </label>
+  <input class="h-13 w-1/4 px-2.5 text-xl border border-gray-400
+        focus:border-emerald-500 focus:outline-none mb-6" v-bind="$attrs"
+        v-model="modelValue" :placeholder="label" />
+</template>
--- src/views/event/EventFormView.vue
+++ src/views/event/EventFormView.vue
@@ -2,6 +2,7 @@
 import type { Event } from '@/types'
 import { ref } from 'vue'
 import EventService from '@/services/EventService'
+import BaseInput from '@/components/BaseInput.vue'
 import { useRouter } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 
@@ -14,10 +15,7 @@ const event = ref<Event>({
   date: '',
   time: '',
   petsAllowed: false,
-  organizer: {
-    id: 0,
-    name: ''
-  }
+  organizer: { id: 0, name: '' }
 })
 
 const router = useRouter()
@@ -42,17 +40,12 @@ function saveEvent() {
   <div>
     <h1>Create an event</h1>
     <form @submit.prevent="saveEvent">
+      <BaseInput v-model="event.category" type="text" label="Category" />
       <h3>Name & describe your event</h3>
-      <label class="block text-gray-500 font-bold">Category</label>
-      <input v-model="event.category" type="text" placeholder="Category" class="h-13 w-full px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6" />
-      <h3>Name & describe your event</h3>
-      <label class="block text-gray-500 font-bold">Title</label>
-      <input v-model="event.title" type="text" placeholder="Title" class="h-13 w-1/4 px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6" />
-      <label class="block text-gray-500 font-bold">Description</label>
-      <input v-model="event.description" type="text" placeholder="Description" class="h-13 w-1/4 px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6" />
+      <BaseInput v-model="event.title" type="text" label="Title" />
+      <BaseInput v-model="event.description" type="text" label="Description" />
       <h3>Where is your event?</h3>
-      <label class="block text-gray-500 font-bold">Location</label>
-      <input v-model="event.location" type="text" placeholder="Location" class="h-13 w-1/4 px-2.5 text-xl border border-gray-400 focus:border-emerald-500 focus:outline-none mb-6" />
+      <BaseInput v-model="event.location" type="text" label="Location" />
       <button class="flex w-fit mx-auto items-center justify-center h-13 px-10 rounded-md font-semibold whitespace-nowrap border border-gray-400 focus:border-emerald-500 transition-all duration-200 ease-linear hover:scale-105 hover:border-emerald-500 hover:shadow-lg active:scale-100 focus:outline-none" type="submit">Submit</button>
     </form>
 
